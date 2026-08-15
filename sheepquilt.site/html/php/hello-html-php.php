58a58db4a80d1fb026e8341f46cd64abe027c165
school email <shw146@ucsd.edu>
2026-08-15 10:26:04 -0700

    edited php hello file

--- sheepquilt.site/html/php/hello-html-php.php
+++ sheepquilt.site/html/php/hello-html-php.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>hello from php</title>
+</head>
+<body>
 <?php
-echo "<h1>This is a php file</h1>"
+$now = new DateTime();
+$name = "Shawn";
+$language = "php";
+echo "<h1>Hello from $name!</h1>";
+echo "<p>This file is being served using $language</p>";
+echo "<p>$now->format('Y-m-d H:i:s')</p>";
+echo "<p>Your IP address is: $_SERVER['REMOTE_ADDR']</p>";
 ?>
+</body>
+</html>
