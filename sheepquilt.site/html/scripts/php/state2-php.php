f89d076ca728760817cef52868b5337fe6ed49eb
school email <shw146@ucsd.edu>
2026-08-18 18:51:02 -0700

    finished php -> debugging

---
+++ sheepquilt.site/html/scripts/php/state2-php.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if (isset($_SESSION["name"])) {
+    echo "<h1>State Check 2</h1>";
+    echo "<p>The name you inputted is: ".htmlspecialchars($_SESSION["name"])."</p>";
+    echo "<a href='/scripts/php/state1-php.php'>session page 1</a>";
+    echo "<form action=/scripts/php/destroy-session.php' method='get'>";
+    echo "<button type='submit'>Destroy Session</button>";
+} else {
+    echo "No session data found.";
+    echo "<a href='/scripts/php/state1-php.php'>session page 1</a>";
+    echo "<form action=/scripts/php/destroy-session.php' method='get'>";
+    echo "<button type='submit'>Destroy Session</button>";
+}
+?>
