5717b7de0ab6941f31c705a42a817926079647d4
school email <shw146@ucsd.edu>
2026-08-18 18:59:47 -0700

    fixing destroy php url issues

--- sheepquilt.site/html/scripts/php/state1-php.php
+++ sheepquilt.site/html/scripts/php/state1-php.php
@@ -12,6 +12,6 @@ if (!isset($_SESSION["name"])) {
 echo "<h1>State Check</h1>";
 echo "<p>The name you inputted is: ".htmlspecialchars($_SESSION["name"])."</p>";
 echo "<a href='/scripts/php/state2-php.php'>session page 2</a>";
-echo "<form action=/scripts/php/destroy-session.php' method='get'>";
+echo "<form action='/scripts/php/destroy-session.php' method='get'>";
 echo "<button type='submit'>Destroy Session</button>";
 ?>
