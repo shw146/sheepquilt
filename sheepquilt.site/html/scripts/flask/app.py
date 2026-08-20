356205f491fc078f11cd3265b427267a3e932239
school email <shw146@ucsd.edu>
2026-08-19 19:45:20 -0700

    created hello html flask

--- sheepquilt.site/html/scripts/flask/app.py
+++ sheepquilt.site/html/scripts/flask/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from hello_html_flask import get_html
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "<h1>Flask is working!</h1>"
+
+@app.route("/hello-html-flask")
+def hello_html_flask():
+    return get_html()
