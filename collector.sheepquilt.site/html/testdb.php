3d9dd368be7f91a320bee4531bd1de03fb456d16
school email <shw146@ucsd.edu>
2026-08-28 12:50:31 -0700

    updated db test for debugging

--- collector.sheepquilt.site/html/testdb.php
+++ collector.sheepquilt.site/html/testdb.php
@@ -1,13 +1,14 @@
 <?php
 $host = "localhost";
 $user = "shawnwang";
-$db = "userTracking";
+$db = "usertracking";
 
 // Open connection to PostgreSQL
 $conn = pg_connect("host=$host dbname=$db user=$user");
 
 if (!$conn) {
     echo "❌ Connection failed.";
+    echo pg_last_error();
 } else {
     echo "✅ Successfully connected to PostgreSQL from Apache!";
 }
