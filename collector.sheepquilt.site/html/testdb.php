58783b1e38bf8e10d725ad7cc8fbe8f79120b68e
school email <shw146@ucsd.edu>
2026-08-28 12:56:40 -0700

    updated db test with password; couldn't bypass otherwise

--- collector.sheepquilt.site/html/testdb.php
+++ collector.sheepquilt.site/html/testdb.php
@@ -2,9 +2,10 @@
 $host = "localhost";
 $user = "shawnwang";
 $db = "usertracking";
+$password = "123456789";
 
 // Open connection to PostgreSQL
-$conn = pg_connect("host=$host dbname=$db user=$user");
+$conn = pg_connect("host=$host dbname=$db user=$user password=$password");
 
 if (!$conn) {
     echo "❌ Connection failed.";
