3d245e49d978c433ffcf313a7d4ef2414a973441
school email <shw146@ucsd.edu>
2026-09-01 14:40:16 -0700

    file renaming and printing all user info for admin page

---
+++ reporting.sheepquilt.site/html/api/users.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    if (!isset($_SESSION['username'])) {
+        header("Location: /api/login.php");
+        exit;
+    }
+
+    if ($_SESSION['permission'] !== 'admin') {
+        http_response_code(403);
+        die("Access denied.");
+    }
+?>
+
+<?php
+    $host = "localhost";
+    $dbname = "usertracking";
+    $dbuser = "shawnwang";
+    $dbpass = "123456789";
+
+    $pdo = new PDO(
+        "pgsql:host=$host;dbname=$dbname",
+        $username,
+        $password
+    );
+
+    $stmt = $pdo->query("SELECT username, password, permission FROM users");
+
+    while($row = $stmt -> fetch(PDO::FETCH_ASSOC)){
+        echo "<p> username: ".htmlspecialchars($row['username'])."</p>";
+        echo "<p> username: ".htmlspecialchars($row['password'])."</p>";
+        echo "<p> permission: ".htmlspecialchars($row['permission'])."</p>";
+    }
+?>
